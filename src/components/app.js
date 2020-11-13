0714cc495464dc81288659de0fd3bbf5248d2fff
Michael Threels <michaelthreels@gmail.com>
2020-11-13 23:52:11 +0100

    feat: added logic to the calculate module and operate function

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './button_panel';
+import Calculate from '../logic/calculate';
 
 export default function App() {
+  Calculate();
   return (
     <>
       <Display />
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,24 @@
 import Big from 'big.js';
+
+export default function Operate(numberOne, numberTwo, operation) {
+  const numOne = Big(numberOne);
+  const numTwo = Big(numberTwo);
+  let result;
+  switch (operation) {
+    case '+':
+      result = numOne.plus(numTwo);
+      break;
+    case '-':
+      result = numOne.minus(numTwo);
+      break;
+    case '*':
+      result = numOne.times(numTwo);
+      break;
+    case '÷':
+      result = numOne.div(numTwo);
+      break;
+    default:
+      break;
+  }
+  return result;
+}
