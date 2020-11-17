e43e6b4779cbc5d8cc02235b4ae6d69849cae860
Michael Threels <michaelthreels@gmail.com>
2020-11-17 20:49:38 +0100

    feat: refactored the App class component to a functional component using hooks

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,34 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './display';
 import ButtonPanel from './button_panel';
 import Calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [state, setState] = useState('black');
 
-  handleClick(buttonName) {
-    const updateData = Calculate(this.state, buttonName);
-    this.setState(updateData);
-  }
-
-  render() {
-    const { total, next } = this.state;
-
-    return (
-      <div>
-        <Display amount={next || total || '0'} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
+  const handleClick = buttonName => {
+    const updateData = Calculate(state, buttonName);
+    setState(updateData);
+  };
+  return (
+    <div>
+      <Display amount={state.next || state.total || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
